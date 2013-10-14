30678c59859740d44592e608104623d3a05dc982
Neal Lathia <neal.lathia@cl.cam.ac.uk>
2013-10-14 10:48:43 +0100

    adding get methods

--- src/com/lathia/profilemanager/db/tables/sequence/EventMapTable.java
+++ src/com/lathia/profilemanager/db/tables/sequence/EventMapTable.java
@@ -1,11 +1,15 @@
 package com.lathia.profilemanager.db.tables.sequence;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.lathia.profilemanager.db.tables.AbstractTable;
@@ -52,6 +56,73 @@ public class EventMapTable extends AbstractTable
 		content.put(timeStamp, entryTime);
 		content.put(variableMap, data.toString());
 		database.insert(tableName, null, content);
+	}
+	
+	public List<JSONObject> getJSONEvents(final SQLiteDatabase database, final int daysInPast)
+	{
+		ArrayList<JSONObject> events = new ArrayList<JSONObject>();
+		try
+		{
+			long one_day = 1000L * 60 * 60 * 24;
+			long timeLimit = System.currentTimeMillis() - (daysInPast * one_day);
+			Cursor cursor = database.query(tableName, new String[]{variableMap}, timeStamp+" > ", new String[]{""+timeLimit}, null, null, null);
+			if (cursor != null)
+			{
+				int eventIndex = cursor.getColumnIndex(variableMap);
+				cursor.moveToFirst();
+				while (!cursor.isAfterLast())
+				{
+					try
+					{
+						String event = cursor.getString(eventIndex);
+						events.add(new JSONObject(event));
+					}
+					catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+					cursor.moveToNext();
+				}
+				cursor.close();
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return events;
+	}
+	
+	public List<HashMap<String, String>> getEvents(final SQLiteDatabase database, final int daysInPast)
+	{
+		List<JSONObject> jsonEvents = getJSONEvents(database, daysInPast);
+		ArrayList<HashMap<String, String>> events = new ArrayList<HashMap<String, String>>();
+		for (JSONObject jsonEvent : jsonEvents)
+		{
+			HashMap<String, String> map = toMap(jsonEvent);
+			events.add(map);
+		}
+		return events;
+	}
+	
+	private HashMap<String, String> toMap(final JSONObject json)
+	{
+		HashMap<String, String> map = new HashMap<String, String>();
+		@SuppressWarnings("unchecked")
+		Iterator<String> keys = json.keys();
+		while (keys.hasNext())
+		{
+			try
+			{
+				String key = keys.next();
+				map.put(key, json.getString(key));
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		return map;
 	}
 	
 	private JSONObject toJSON(final HashMap<String, String> values)
