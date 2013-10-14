11accaee0f9f27cdf46d876196e7143e8d704be5
Neal Lathia <neal.lathia@cl.cam.ac.uk>
2013-10-14 09:52:31 +0100

    implementing add methods

--- src/com/lathia/profilemanager/db/tables/sequence/EventMapTable.java
+++ src/com/lathia/profilemanager/db/tables/sequence/EventMapTable.java
@@ -1,8 +1,14 @@
 package com.lathia.profilemanager.db.tables.sequence;
 
-import com.lathia.profilemanager.db.tables.AbstractTable;
+import java.util.HashMap;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
+
+import com.lathia.profilemanager.db.tables.AbstractTable;
 
 public class EventMapTable extends AbstractTable
 {
@@ -24,67 +30,44 @@ public class EventMapTable extends AbstractTable
 				+ ");");
 	}
 	
-//	public void incrementField(final SQLiteDatabase database, final String variable, final int amount)
-//	{
-//		ContentValues values = new ContentValues();
-//		int currentFrequency = getCurrentFrequency(database, variable);
-//		if (currentFrequency == NOT_FOUND)
-//		{
-//			values.put(variableName, variable);
-//			values.put(variableFrequency, amount);
-//			database.insert(tableName, null, values);
-//		}
-//		else
-//		{
-//			values.put(variableFrequency,  currentFrequency + amount);
-//			database.update(tableName, values, variableName+" = ?", new String[]{variable});
-//		}
-//	}
-//	
-//	private int getCurrentFrequency(final SQLiteDatabase database, final String variable)
-//	{
-//		try
-//		{
-//			Cursor cursor = database.query(tableName, new String[]{variableFrequency}, variableName+" = ? ", new String[]{variable}, null, null, null);
-//			if (cursor != null)
-//			{
-//				int frequency;
-//				if (cursor.getCount() > 0)
-//				{
-//					cursor.moveToFirst();
-//					frequency = cursor.getInt(cursor.getColumnIndex(variableFrequency));
-//				}
-//				else
-//				{
-//					frequency = NOT_FOUND;
-//				}
-//				cursor.close();
-//				return frequency;
-//			}
-//		}
-//		catch (Exception e)
-//		{}
-//		return NOT_FOUND;
-//	}
-//	
-//	public Distribution getDistribution(final SQLiteDatabase database)
-//	{
-//		Distribution result = new Distribution();
-//		Cursor cursor = database.query(tableName, null, null, null, null, null, null);
-//		if (cursor != null)
-//		{
-//			int variableNameIndex = cursor.getColumnIndex(variableName);
-//			int variableValueIndex = cursor.getColumnIndex(variableFrequency);
-//			cursor.moveToFirst();
-//			while (!cursor.isAfterLast())
-//			{
-//				String key = cursor.getString(variableNameIndex);
-//				Integer frequency = cursor.getInt(variableValueIndex);
-//				result.put(key, frequency);
-//				cursor.moveToNext();
-//			}
-//			cursor.close();
-//		}
-//		return result;
-//	}
+	public void add(final SQLiteDatabase database, final HashMap<String, String> values)
+	{
+		add(database, System.currentTimeMillis(), values);
+	}
+	
+	public void add(final SQLiteDatabase database, final long entryTime, final HashMap<String, String> values)
+	{
+		JSONObject data = toJSON(values);
+		add(database, entryTime, data);
+	}
+	
+	public void add(final SQLiteDatabase database, final JSONObject data)
+	{
+		add(database, System.currentTimeMillis(), data);
+	}
+	
+	public void add(final SQLiteDatabase database, final long entryTime, final JSONObject data)
+	{
+		ContentValues content = new ContentValues();
+		content.put(timeStamp, entryTime);
+		content.put(variableMap, data.toString());
+		database.insert(tableName, null, content);
+	}
+	
+	private JSONObject toJSON(final HashMap<String, String> values)
+	{
+		JSONObject data = new JSONObject();
+		for (String key : values.keySet())
+		{
+			try
+			{
+				data.put(key, values.get(key));
+			}
+			catch (JSONException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		return data;
+	}
 }
